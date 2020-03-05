0870a5ba9a248cd4ffdebee58e0401d598f9e07a
maloboeda <61827490+maloboeda@users.noreply.github.com>
2020-03-05 11:21:06 +0100

    Add files via upload

---
+++ App/header/Satellite_Artificiel.h
@@ -0,0 +1,16 @@
+
+#ifndef __Satellite_Artificiel_h__
+#define __Satellite_Artificiel_h__
+
+#include "Satellite.h"
+#include "Objet_Artificiel.h"
+
+// class Satellite;
+// class Objet_Artificiel;
+class Satellite_Artificiel;
+
+class Satellite_Artificiel: public Satellite, public Objet_Artificiel
+{
+};
+
+#endif
---
+++ App/source/Satellite_Artificiel.cpp
@@ -0,0 +1,5 @@
+
+#include "Satellite_Artificiel.h"
+#include "Satellite.h"
+#include "Objet_Artificiel.h"
+
